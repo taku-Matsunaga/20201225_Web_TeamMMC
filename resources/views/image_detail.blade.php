e6591aedc49dbf45a18b7cdc921782159044181e
taku-Matsunaga <skyline199306@yahoo.co.jp>
2020-12-23 18:06:17 +0900

    fix blade.php html layout

--- resources/views/image_detail.blade.php
+++ resources/views/image_detail.blade.php
@@ -1,6 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
 @foreach($images as $image)
-<div style="width: 18rem; float:left; margin: 16px; cursor: pointer;">
+<div style="width: 18rem; float:left; margin: 16px;">
     <img src="{{ asset('/storage/' . $image->file_path) }}" style="width:100%;"/>
 
     {{-- 確認var_dump --}}
@@ -11,5 +20,12 @@
     <p>{{ $image->post_by }}</p>
     <p>{{ $image->file_title }}</p>
     <p>{{ $image->file_text }}</p>
+
+    <div>
+        <a href='http://localhost/20201225_Web_TeamMMC/public/list'>リストに戻る</a>
+    </div>
 </div>
 @endforeach
+
+</body>
+</html>
--- resources/views/image_list.blade.php
+++ resources/views/image_list.blade.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
 
 <a href="{{ route('upload_form') }}">Upload</a>
 
@@ -29,3 +39,6 @@
     <p>{{ $image->post_by}}</p>
 </a>
 @endforeach
+
+</body>
+</html>
