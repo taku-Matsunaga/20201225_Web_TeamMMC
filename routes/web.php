3750ee8bc7cf6970116926d249a57ef147da5eab
maru_maru3 <maru.biz@gmail.com>
2020-12-24 12:50:01 +0900

    初回のPUSH

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,7 @@ Route::post(
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
