36e104cf2a98b4349f6a0ed726f7977a19685b8e
taku-Matsunaga <65112145+taku-Matsunaga@users.noreply.github.com>
2020-12-24 16:44:03 +0900

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/register.css') }}" rel="stylesheet">
-    <title>会員登録画面</title>
-</head>
-
-<body>
-
-
-
 @extends('layouts.app')
 
 @section('content')
@@ -90,6 +75,3 @@
     </div>
 </div>
 @endsection
-
-</body>
-</html>
