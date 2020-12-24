6c154d80595ed5cc222751b4b3be8ddd954aaf40
taku-Matsunaga <65112145+taku-Matsunaga@users.noreply.github.com>
2020-12-24 16:51:10 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,3 @@ Route::post(
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
